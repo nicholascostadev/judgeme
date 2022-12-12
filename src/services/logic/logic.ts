1a03c129a877c41eabccfb361ac15990a17a7b32
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-11 21:53:46 -0300

    🚨 Fix lint warnings

--- src/services/logic/logic.ts
+++ src/services/logic/logic.ts
@@ -1,116 +1,116 @@
 export const dynamicUserRepoPhrase = (numOfRepos: number): string => {
-	let userReposPhrase = '';
+  let userReposPhrase = ''
 
-	if (numOfRepos === 0) {
-		userReposPhrase = 'No public repos 😔';
-	} else if (numOfRepos < 25) {
-		userReposPhrase = "You're a normal person, congrats 👏";
-	} else if (numOfRepos < 32) {
-		userReposPhrase =
-			"You probably should focus on only one repo, don't you think so? ";
-	} else if (numOfRepos < 43) {
-		userReposPhrase = "Don't you think you're a bit too much of a geek? 🤔";
-	} else {
-		userReposPhrase = 'Are you freaking kidding me? Like, really?😶';
-	}
+  if (numOfRepos === 0) {
+    userReposPhrase = 'No public repos 😔'
+  } else if (numOfRepos < 25) {
+    userReposPhrase = "You're a normal person, congrats 👏"
+  } else if (numOfRepos < 32) {
+    userReposPhrase =
+      "You probably should focus on only one repo, don't you think so? "
+  } else if (numOfRepos < 43) {
+    userReposPhrase = "Don't you think you're a bit too much of a geek? 🤔"
+  } else {
+    userReposPhrase = 'Are you freaking kidding me? Like, really?😶'
+  }
 
-	return userReposPhrase;
-};
+  return userReposPhrase
+}
 
 export const dynamicContributionPhrase = (numOfContribs: number): string => {
-	let userContribsPhrase = '';
+  let userContribsPhrase = ''
 
-	if (numOfContribs === 0) {
-		userContribsPhrase = 'No contributions 😔';
-	} else if (numOfContribs < 25) {
-		userContribsPhrase = "You're a normal person, congrats 👏";
-	} else if (numOfContribs < 100) {
-		userContribsPhrase = "Sorry for asking, but don't you have a life?";
-	} else if (numOfContribs < 500) {
-		userContribsPhrase =
-			"You're so good that I would risk to say you use VIM as your editor";
-	} else {
-		userContribsPhrase = 'Are you freaking kidding me? Like, really?😶';
-	}
-	return userContribsPhrase;
-};
+  if (numOfContribs === 0) {
+    userContribsPhrase = 'No contributions 😔'
+  } else if (numOfContribs < 25) {
+    userContribsPhrase = "You're a normal person, congrats 👏"
+  } else if (numOfContribs < 100) {
+    userContribsPhrase = "Sorry for asking, but don't you have a life?"
+  } else if (numOfContribs < 500) {
+    userContribsPhrase =
+      "You're so good that I would risk to say you use VIM as your editor"
+  } else {
+    userContribsPhrase = 'Are you freaking kidding me? Like, really?😶'
+  }
+  return userContribsPhrase
+}
 
 type Repo = {
-	id: number;
-	name: string;
-	owner: {
-		login: string;
-	};
-	html_url: string;
-	description?: string;
-	created_at: string;
-	updated_at: string;
-	stargazers_count: number;
-	homepage?: string;
-	language: string;
-};
+  id: number
+  name: string
+  owner: {
+    login: string
+  }
+  html_url: string
+  description?: string
+  created_at: string
+  updated_at: string
+  stargazers_count: number
+  homepage?: string
+  language: string
+}
 
-type RepositoryProps = Repo[] | undefined;
+type RepositoryProps = Repo[] | undefined
 
 type getUserMostUsedLanguagesReturn = [
-	{ language: string; percentage: number }[],
-	string[]
-];
+  { language: string; percentage: number }[],
+  string[],
+]
 
 export const getUserMostUsedLanguage = (
-	repos: RepositoryProps
+  repos: RepositoryProps,
 ): getUserMostUsedLanguagesReturn => {
-	const parsedArray = JSON.parse(String(repos));
-	const languages = parsedArray?.map((repo: Repo) => repo.language);
-	const filteredLanguages = languages?.filter(
-		(language: string) => language !== null && language !== 'null'
-	);
-	const languageCount = filteredLanguages?.reduce((acc: any, curr: any) => {
-		if (acc[curr]) {
-			acc[curr]++;
-		} else {
-			acc[curr] = 1;
-		}
-		return acc;
-	}, {});
+  const parsedArray = JSON.parse(String(repos))
+  const languages = parsedArray?.map((repo: Repo) => repo.language)
+  const filteredLanguages = languages?.filter(
+    (language: string) => language !== null && language !== 'null',
+  )
+  const languageCount = filteredLanguages?.reduce((acc: any, curr: any) => {
+    if (acc[curr]) {
+      acc[curr]++
+    } else {
+      acc[curr] = 1
+    }
+    return acc
+  }, {})
 
-	const languagePercentage = Object.keys(languageCount).reduce(
-		(acc: any, curr: any) => {
-			acc[curr] = (languageCount[curr] / languages.length) * 100;
-			return acc;
-		},
-		{}
-	);
+  const languagePercentage = Object.keys(languageCount).reduce(
+    (acc: any, curr: any) => {
+      acc[curr] = (languageCount[curr] / languages.length) * 100
+      return acc
+    },
+    {},
+  )
 
-	const sortedLanguages = Object.keys(languagePercentage).reduce(
-		(acc: any, curr: any) => {
-			acc[curr] = languagePercentage[curr];
-			return acc;
-		},
-		{}
-	);
+  const sortedLanguages = Object.keys(languagePercentage).reduce(
+    (acc: any, curr: any) => {
+      acc[curr] = languagePercentage[curr]
+      return acc
+    },
+    {},
+  )
 
-	const sortedLanguagesArray = Object.keys(sortedLanguages).reduce(
-		(acc: any, curr: any) => {
-			acc.push({
-				language: curr,
-				percentage: sortedLanguages[curr],
-			});
-			return acc;
-		},
-		[]
-	);
+  const sortedLanguagesArray = Object.keys(sortedLanguages).reduce(
+    (acc: any, curr: any) => {
+      acc.push({
+        language: curr,
+        percentage: sortedLanguages[curr],
+      })
+      return acc
+    },
+    [],
+  )
 
-	const sortedLanguagesArraySorted = sortedLanguagesArray.sort(
-		(a: any, b: any) => b.percentage - a.percentage
-	);
+  const sortedLanguagesArraySorted = sortedLanguagesArray.sort(
+    (a: any, b: any) => b.percentage - a.percentage,
+  )
 
-	const topFiveLanguages = sortedLanguagesArraySorted.slice(0, 5);
+  const topFiveLanguages = sortedLanguagesArraySorted.slice(0, 5)
 
-	// get top 5 languages as array of strings
-	const topFiveLanguagesArray = topFiveLanguages.map(
-		(language: any) => language.language
-	);
+  // get top 5 languages as array of strings
+  const topFiveLanguagesArray = topFiveLanguages.map(
+    (language: any) => language.language,
+  )
 
-	return [sortedLanguagesArraySorted, topFiveLanguagesArray];
-};
+  return [sortedLanguagesArraySorted, topFiveLanguagesArray]
+}

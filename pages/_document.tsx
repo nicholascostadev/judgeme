4529694a36fdddaf03d5df0ae9fa0f81cd326aa5
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-12 00:46:40 -0300

    ✨ add _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+// generate _document template file
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Judgeme" />
+          <meta name="keywords" content="Judgeme" />
+          <meta name="author" content="Judgeme" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
